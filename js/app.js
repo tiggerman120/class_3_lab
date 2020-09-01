c3d5fb0ed04a26270d80b3f9f42f33928bbba5df
Garrett Cintron <grrttcintron@gmail.com>
2020-09-01 14:37:48 -0700

    adding my lab so far to test on live-server

--- js/app.js
+++ js/app.js
@@ -1,3 +1,43 @@
-'use strict';
+'use script'
 
-console.log('js is loaded');
+var userName = prompt('Please enter your name');
+    alert('Hello ' + userName);
+    if (userName === 'Garrett') {
+        document.write('<h2>' + 'Your name is Toby, boy ' + userName + '</h2>')
+    } else {alert('Hello ' + userName + '!');
+    }
+
+    var answer1 = 'kentucky'
+    var answer2 = '26'
+    var answer3 = 'Maryland'
+    var answer4 = 'skilled nursing facility(SNF)'
+    var answer5 = 'hiking'
+
+    var homeTown = prompt('Was I born in kentucky?');
+        if (homeTown = 'yes') {
+            console.log('Correct! I was born in ' + answer1, answer1.toLowerCase);
+            alert('Correct! I was born in ' + answer1, answer1.toLowerCase);
+        } else {
+            console.log('Incorrect! I was born in ' + answer1, answer1.toLowerCase + 'Better luck next time!');
+            alert('Incorrect! I was born in ' + answer1, answer1.toLowerCase + 'better luck next time!');
+        }
+    var age = prompt('Am I 26 years old?');
+    if (age = 'yes') {
+        console.log('Correct! I was born in ' + answer2, answer2.toLowerCase);
+        alert('Correct! I was born in ' + answer2, answer2.toLowerCase);
+    } else {
+        console.log('Incorrect! I was born in ' + answer2, answer2.toLowerCase + 'Better luck next time!');
+        alert('Incorrect! I was born in ' + answer2, answer2.toLowerCase + 'better luck next time!');
+    }
+        
+        
+
+    var raisedState = prompt('Was I raised in Virginia?');
+    
+
+    var jobWork = prompt('Do I work in an skilled nursing facility(SNF)?');
+    
+
+    var hobby = prompt('Is hiking one of my hobbies?');
+
+    
