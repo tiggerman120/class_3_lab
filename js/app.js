559882c135349a1bb785ebb9530e6d20ec1ce8e2
Garrett Cintron <grrttcintron@gmail.com>
2020-09-01 18:19:16 -0700

    finished building loops for prompts. need to ask if i can put my loops inside of functions and if that will allow me to reuse the answeredProperly variable. added some css. minor corrections.

--- js/app.js
+++ js/app.js
@@ -16,12 +16,14 @@ if (ready === true) {
   var answeredProperly = false;
   while (answeredProperly === false) {
     var answer1 = prompt('Was I born in kentucky? yes/no');
-    console.log(answer1.toLowerCase());
+    //console.log(answer1.toLowerCase());
     if (answer1.toLowerCase() === 'yes') {
       answeredProperly = true;
+      //console.log(answer1.toLowerCase());
       alert('Correct! I was born in kentucky');
     } else if (answer1.toLowerCase() === 'no') {
       answeredProperly = true;
+      //console.log(answer1.toLowerCase());
       alert('Incorrect! I was indeed born in kentucky!');
     } else {
       alert('Please pick yes or no!');
@@ -30,47 +32,75 @@ if (ready === true) {
   }
 }
 
+var answeredProperly2 = false;
+while (answeredProperly2 === false) {
+  var answer2 = prompt('Am I 26 years old? yes/no');
+  //console.log(answer2.toLowerCase());
+  if (answer2.toLowerCase() === 'yes') {
+    answeredProperly2 = true;
+    //console.log(answer2.toLowerCase());
+    alert('Correct! I am 26 years old!');
+  } else if (answer2.toLowerCase() === 'no') {
+    answeredProperly2 = true;
+    //console.log(answer2.toLowerCase());
+    alert('Incorrect! I ackshually am 26 years old!');
+  } else {
+    alert('Please pick yes or no!');
 
-var answer2 = prompt('Am I 26 years old? yes/no');
-console.log(answer2.toLowerCase());
-if (answer2.toLowerCase() === 'yes') {
-  alert('Correct! I am 26 years old!');
-} else if (answer2.toLowerCase() === 'no') {
-  alert('Incorrect! I ackshually am 26 years old!');
-} else {
-  alert('Please pick yes or no!');
+  }
+}
+
+var answeredProperly3 = false;
+while (answeredProperly3 === false) {
+  var answer3 = prompt('Was I raised in Virginia? yes/no');
+  //console.log(answer3.toLowerCase());
+  if (answer3.toLowerCase() === 'no') {
+    answeredProperly3 = true;
+    //console.log(answer3.toLowerCase());
+    alert('Correct! I was raised in Maryland');
+  } else if (answer3.toLowerCase() === 'yes') {
+    answeredProperly3 = true;
+    //console.log(answer3.toLowerCase());
+    alert('Incorrect! I ackshually was raised in Maryland!');
+  } else {
+    alert('Please pick yes or no!');
+
+  }
 }
 
 
+var answeredProperly4 = false;
+while (answeredProperly4 === false) {
+  var answer4 = prompt('Am I employed as a nurses assistant?');
+  //console.log(answer4.toLowerCase());
+  if (answer4.toLowerCase() === 'yes') {
+    answeredProperly4 = true;
+    //console.log(answer4.toLowerCase());
+    alert('exactamundo! I work as a nurses assistant!');
+  } else if (answer4.toLowerCase() === 'no') {
+    answeredProperly4 = true;
+    //console.log(answer4.toLowerCase());
+    alert('you so wrong! I definitely work as a nurses assistant!');
+  } else {
+    alert('Please pick yes or no!');
 
-var answer3 = prompt('Was I raised in Virginia? yes/no');
-console.log(answer3.toLowerCase());
-if (answer3.toLowerCase() === 'no') {
-  alert('Correct! I was raised in Maryland');
-} else if (answer3.toLowerCase() === 'yes') {
-  alert('Incorrect! I ackshually was raised in Maryland!');
-} else {
-  alert('Please pick yes or no!');
+  }
 }
 
+var answeredProperly5 = false;
+while (answeredProperly5 === false) {
+  var answer5 = prompt('Is hiking one of my hobbies');
+  //console.log(answer5.toLowerCase());
+  if (answer3.toLowerCase() === 'yes') {
+    answeredProperly5 = true;
+    //console.log(answer5.toLowerCase());
+    alert('Awesome job! It is totally my favorite hobby, I love the woods!');
+  } else if (answer5.toLowerCase() === 'no') {
+    answeredProperly5 = true;
+    //console.log(answer5.toLowerCase());
+    alert('No no no no no no no! Hiking is like my favorite thing ever!');
+  } else {
+    alert('Please pick yes or no!');
 
-var answer4 = prompt('Am I employed as a nursing assistant?');
-console.log(answer4.toLowerCase());
-if (answer4.toLowerCase() === 'yes') {
-  alert('Correctamundo! I am employed as a nursing assistant!!');
-} else if (answer4.toLowerCase() === 'no') {
-  alert('You so wrong! I am employed as a nursing assistant!');
-} else {
-  alert('Please pick yes or no!');
-}
-
-
-var answer5 = prompt('Is hiking one of my hobbies?');
-console.log(answer5.toLowerCase());
-if (answer5.toLowerCase() === 'yes') {
-  alert('Awesome job! It is totally my favorite hobby, I love the woods!');
-} else if (answer5.toLowerCase() === 'no') {
-  alert('No no no no no no no! Hiking is like my favorite thing ever!');
-} else {
-  alert('Please pick yes or no!');
+  }
 }
