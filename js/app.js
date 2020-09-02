356cff3336ce9824f5412649a1865a7f206bd653
Garrett Cintron <grrttcintron@gmail.com>
2020-09-01 17:37:12 -0700

    created a loop on first prompt. debugged js. debugged html. added to css. filled out my prompts.

--- js/app.js
+++ js/app.js
@@ -1,43 +1,76 @@
-'use script'
+'use strict';
+console.log('file working');
 
 var userName = prompt('Please enter your name');
-    alert('Hello ' + userName);
-    if (userName === 'Garrett') {
-        document.write('<h2>' + 'Your name is Toby, boy ' + userName + '</h2>')
-    } else {alert('Hello ' + userName + '!');
-    }
+if (userName === 'Garrett') {
+  alert('Your name is Toby, ' + userName);
+} else {alert('Hello ' + userName + ' Welcome to Narnia!' + ' Do you like winter?');
+}
 
-    var answer1 = 'kentucky'
-    var answer2 = '26'
-    var answer3 = 'Maryland'
-    var answer4 = 'skilled nursing facility(SNF)'
-    var answer5 = 'hiking'
+var ready = confirm('Are you ready');
 
-    var homeTown = prompt('Was I born in kentucky?');
-        if (homeTown = 'yes') {
-            console.log('Correct! I was born in ' + answer1, answer1.toLowerCase);
-            alert('Correct! I was born in ' + answer1, answer1.toLowerCase);
-        } else {
-            console.log('Incorrect! I was born in ' + answer1, answer1.toLowerCase + 'Better luck next time!');
-            alert('Incorrect! I was born in ' + answer1, answer1.toLowerCase + 'better luck next time!');
-        }
-    var age = prompt('Am I 26 years old?');
-    if (age = 'yes') {
-        console.log('Correct! I was born in ' + answer2, answer2.toLowerCase);
-        alert('Correct! I was born in ' + answer2, answer2.toLowerCase);
+
+console.log(ready);
+
+if (ready === true) {
+  var answeredProperly = false;
+  while (answeredProperly === false) {
+    var answer1 = prompt('Was I born in kentucky? yes/no');
+    console.log(answer1.toLowerCase());
+    if (answer1.toLowerCase() === 'yes') {
+      answeredProperly = true;
+      alert('Correct! I was born in kentucky');
+    } else if (answer1.toLowerCase() === 'no') {
+      answeredProperly = true;
+      alert('Incorrect! I was indeed born in kentucky!');
     } else {
-        console.log('Incorrect! I was born in ' + answer2, answer2.toLowerCase + 'Better luck next time!');
-        alert('Incorrect! I was born in ' + answer2, answer2.toLowerCase + 'better luck next time!');
+      alert('Please pick yes or no!');
+
     }
-        
-        
+  }
+}
 
-    var raisedState = prompt('Was I raised in Virginia?');
-    
 
-    var jobWork = prompt('Do I work in an skilled nursing facility(SNF)?');
-    
+var answer2 = prompt('Am I 26 years old? yes/no');
+console.log(answer2.toLowerCase());
+if (answer2.toLowerCase() === 'yes') {
+  alert('Correct! I am 26 years old!');
+} else if (answer2.toLowerCase() === 'no') {
+  alert('Incorrect! I ackshually am 26 years old!');
+} else {
+  alert('Please pick yes or no!');
+}
 
-    var hobby = prompt('Is hiking one of my hobbies?');
 
-    
+
+var answer3 = prompt('Was I raised in Virginia? yes/no');
+console.log(answer3.toLowerCase());
+if (answer3.toLowerCase() === 'no') {
+  alert('Correct! I was raised in Maryland');
+} else if (answer3.toLowerCase() === 'yes') {
+  alert('Incorrect! I ackshually was raised in Maryland!');
+} else {
+  alert('Please pick yes or no!');
+}
+
+
+var answer4 = prompt('Am I employed as a nursing assistant?');
+console.log(answer4.toLowerCase());
+if (answer4.toLowerCase() === 'yes') {
+  alert('Correctamundo! I am employed as a nursing assistant!!');
+} else if (answer4.toLowerCase() === 'no') {
+  alert('You so wrong! I am employed as a nursing assistant!');
+} else {
+  alert('Please pick yes or no!');
+}
+
+
+var answer5 = prompt('Is hiking one of my hobbies?');
+console.log(answer5.toLowerCase());
+if (answer5.toLowerCase() === 'yes') {
+  alert('Awesome job! It is totally my favorite hobby, I love the woods!');
+} else if (answer5.toLowerCase() === 'no') {
+  alert('No no no no no no no! Hiking is like my favorite thing ever!');
+} else {
+  alert('Please pick yes or no!');
+}
