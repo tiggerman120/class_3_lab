4257851a0b42deb39cd03f7ddc18b916cdc14303
Garrett Cintron <grrttcintron@gmail.com>
2020-09-05 18:57:07 -0700

    final touches to the promptpit

--- js/app.js
+++ js/app.js
@@ -13,24 +13,16 @@ function yourName() {
 
 
 bornKy();
-function bornKy() {}
-var ready = confirm('Are you ready');
-console.log(ready);
-if (ready === true) {
+function bornKy() {
   var answeredProperly1 = false;
   while (answeredProperly1 === false) {
     var answer1 = prompt('Was I born in kentucky? yes/no');
-    //console.log(answer1.toLowerCase());
     if (answer1.toLowerCase() === 'yes') {
       answeredProperly1 = true;
-      //correctAnswersTotal = correctAnswersTotal + 1;
-      //correctAnswersTotal += 1
       correctAnswersTotal++;
-      //console.log(answer1.toLowerCase());
       alert('Correct! I was born in kentucky');
     } else if (answer1.toLowerCase() === 'no') {
       answeredProperly1 = true;
-      //console.log(answer1.toLowerCase());
       alert('Incorrect! I was indeed born in kentucky!');
     } else {
       alert('Please pick yes or no!');
@@ -44,15 +36,12 @@ function ageMe() {
   var answeredProperly2 = false;
   while (answeredProperly2 === false) {
     var answer2 = prompt('Am I 26 years old? yes/no');
-    //console.log(answer2.toLowerCase());
     if (answer2.toLowerCase() === 'yes') {
       answeredProperly2 = true;
       correctAnswersTotal++;
-      //console.log(answer2.toLowerCase());
       alert('Correct! I am 26 years old!');
     } else if (answer2.toLowerCase() === 'no') {
       answeredProperly2 = true;
-      //console.log(answer2.toLowerCase());
       alert('Incorrect! I ackshually am 26 years old!');
     } else {
       alert('Please pick yes or no!');
@@ -66,15 +55,12 @@ function raisedMd() {
   var answeredProperly3 = false;
   while (answeredProperly3 === false) {
     var answer3 = prompt('Was I raised in Virginia? yes/no');
-    //console.log(answer3.toLowerCase());
     if (answer3.toLowerCase() === 'no') {
       answeredProperly3 = true;
       correctAnswersTotal++;
-      //console.log(answer3.toLowerCase());
       alert('Correct! I was raised in Maryland');
     } else if (answer3.toLowerCase() === 'yes') {
       answeredProperly3 = true;
-      //console.log(answer3.toLowerCase());
       alert('Incorrect! I ackshually was raised in Maryland!');
     } else {
       alert('Please pick yes or no!');
@@ -88,15 +74,12 @@ function workHist() {
   var answeredProperly4 = false;
   while (answeredProperly4 === false) {
     var answer4 = prompt('Am I employed as a nurses assistant?');
-    //console.log(answer4.toLowerCase());
     if (answer4.toLowerCase() === 'yes') {
       answeredProperly4 = true;
       correctAnswersTotal++;
-      //console.log(answer4.toLowerCase());
       alert('exactamundo! I work as a nurses assistant!');
     } else if (answer4.toLowerCase() === 'no') {
       answeredProperly4 = true;
-      //console.log(answer4.toLowerCase());
       alert('you so wrong! I definitely work as a nurses assistant!');
     } else {
       alert('Please pick yes or no!');
@@ -109,15 +92,12 @@ function loveWoods() {
   var answeredProperly5 = false;
   while (answeredProperly5 === false) {
     var answer5 = prompt('Is hiking one of my hobbies');
-    //console.log(answer5.toLowerCase());
     if (answer5.toLowerCase() === 'yes') {
       answeredProperly5 = true;
       correctAnswersTotal++;
-      //console.log(answer5.toLowerCase());
       alert('Awesome job! It is totally my favorite hobby, I love the woods!');
     } else if (answer5.toLowerCase() === 'no') {
       answeredProperly5 = true;
-      //console.log(answer5.toLowerCase());
       alert('No no no no no no no! Hiking is like my favorite thing ever!');
     } else {
       alert('Please pick yes or no!');
